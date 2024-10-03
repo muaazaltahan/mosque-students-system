b30cc73148d954530d88e0d7b18ed1b98970e936
muaazaltahan <almuaaz559@gmail.com>
2024-10-03 10:44:36 +0300

    delete old project

--- src/app/shared-data/functions..ts
+++
@@ -1,8 +0,0 @@
-export function columnName(key: string) : string {
-  switch (key) {
-    case 'id': return 'الرقم';
-    case 'name': return 'الاسم';
-    case 'attendance': return 'الحضور';
-    default: return '';
-  }
-}
